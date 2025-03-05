449995342a053695335ba367f6a41afb726c1e23
laurabjaime <lauracbarreto@gmail.com>
2025-03-05 23:16:22 +0100

    Update summarize.py

--- summarize.py
+++ summarize.py
@@ -1,10 +1,1 @@
-def add(data):
-    """Function that summarized cols a and b"""
-
-    data["sum"] = data["a"] + data["b"]
-
-    return data
-
-
-if __env:  # indicates we are running on Taktile
-    data = add(data)
+1+1
